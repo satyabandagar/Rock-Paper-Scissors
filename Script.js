53ac33a2e31d27167c037435389569c7af27b589
satyabandagar <bandagarsatyappa8767@gmail.com>
2026-03-04 18:21:56 +0530

    half javaScript completed

--- Script.js
+++ Script.js
@@ -3,3 +3,14 @@ const backroundColorChnage = document.querySelector(".parent");
 function backColor() {
     backroundColorChnage.classList.toggle("parent")
 }
+
+let computerCount = 0;
+let user = 0;
+
+function playGame(userInput){
+
+    const btn = document.getElementById("btn");
+
+    console.log(btn);
+
+}
